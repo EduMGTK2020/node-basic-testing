61b0b10c714030e9bd25aafb9c8100c6b7c97501
Ed <mail@544062.ru>
2023-06-30 13:38:26 +0300

    refactor: add values to 08-snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,19 +1,19 @@
 import { generateLinkedList } from './index';
 
+const values1 = [1, 'Node 2'];
+const values2 = [1, 'Node 2', 'Node 3'];
+
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    const linkedList = generateLinkedList([1, 'Node 2', 'Node 3']);
+    const linkedList = generateLinkedList(values1);
     const expectedLinkedList = {
       value: 1,
       next: {
         value: 'Node 2',
         next: {
-          value: 'Node 3',
-          next: {
-            value: null,
-            next: null,
-          },
+          value: null,
+          next: null,
         },
       },
     };
@@ -22,7 +22,7 @@ describe('generateLinkedList', () => {
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    const linkedList = generateLinkedList([1, 'Node 2', 'Node 3']);
+    const linkedList = generateLinkedList(values2);
     expect(linkedList).toMatchSnapshot();
   });
 });
